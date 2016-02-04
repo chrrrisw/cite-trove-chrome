c148794e3e5555db258f0f22aeb08e1a54322025
Chris Willoughby <chrrrisw@gmail.com>
2016-02-04 12:28:32 +1030

    Comments and removed onConnect listener.

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
 /**
  * @author Chris Willoughby
  */
+
+/**
+ * The message handler for the content script.
+ * If the message is of type 'cite', parse the page and send
+ * the results back to the background script.
+ */
 function processMessage(message, sender, sendResponse) {
     // console.log("message: " + JSON.stringify(message));
     // console.log("sender: " + JSON.stringify(sender));
@@ -13,8 +19,11 @@ function processMessage(message, sender, sendResponse) {
     }
 }
 
+/**
+ * Add the processMessage handler as a listener for messages.
+ */
 chrome.runtime.onMessage.addListener(processMessage);
 
-chrome.runtime.onConnect.addListener(function(port) {
-    console.log("port: " + JSON.stringify(port));
-});
+// chrome.runtime.onConnect.addListener(function(port) {
+//     console.log("port: " + JSON.stringify(port));
+// });
